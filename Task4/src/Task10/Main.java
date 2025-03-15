65f03a51f2c016d41abe3b97c8a5efe7015d1741
Gordeev203 <gordeev.stas2004.s@mail.ru>
2025-03-15 16:15:23 +0300

    Задания 4 раздела

--- Task4/src/Task10/Main.java
+++ Task4/src/Task10/Main.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 10_000_000;
+        int a = 10000_000;
         int b = 1_000_000_000;
 
         System.out.println("Ответ: " + getArithmeticProgressionSum(a, b));
