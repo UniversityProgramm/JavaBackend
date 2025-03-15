0ed9edd6df8fb335ac791f3d0213756ca8055c0a
Gordeev203 <gordeev.stas2004.s@mail.ru>
2025-03-15 16:12:23 +0300

    Задания 4 раздела

---
+++ Task4/src/Task10/Main.java
@@ -0,0 +1,16 @@
+package Task10;
+
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10_000_000;
+        int b = 1_000_000_000;
+
+        System.out.println("Ответ: " + getArithmeticProgressionSum(a, b));
+    }
+
+    public static long getArithmeticProgressionSum(int a, int b) {
+        return IntStream.range(a, b).asLongStream().sum();
+    }
+}
