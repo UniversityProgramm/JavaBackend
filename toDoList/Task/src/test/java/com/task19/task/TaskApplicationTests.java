61c0f8a4253ff9aedbd357bc3b1da5d88d5d463b
Gordeev203 <gordeev.stas2004.s@mail.ru>
2025-04-22 21:26:05 +0300

    ToDoList - Задание от ментора

---
+++ toDoList/Task/src/test/java/com/task19/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task19.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ toDoList/User/src/test/java/com/task19/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task19.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
