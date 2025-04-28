dd3ff44bfcb93be050664ff29f24206bab4bc220
Gordeev203 <gordeev.stas2004.s@mail.ru>
2025-04-28 20:14:47 +0300

    ToDoList - Задание от ментора

--- toDoList/Task/src/main/java/com/toDoList/task/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.toDoList.task.repository;
-
-import com.toDoList.task.model.Task;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends CrudRepository<Task, Long> {
-}
--- toDoList/User/src/main/java/com/toDoList/user/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.toDoList.user.repository;
-
-import com.toDoList.user.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
